c1ee39aaf896e6e7dd62d274e37a8be4cb63f669
Jamie Chang <jamiechang917@gmail.com>
2022-01-25 19:33:48 +0800

    Add files via upload

--- telescope_cover.ino
+++ telescope_cover.ino
@@ -1,6 +1,6 @@
 /* 
  Automatic Telescope Cover
- Board: NodeMCU-32S, Motor: MG-90S
+ Board: NodeMCU-32S, Motor: SG-90
  Author: Jamie Chang 2022/01/18
  Contact: jamiechang917@gmail.com 
  WebServer.h: https://github.com/zhouhan0126/WebServer-esp32.git
@@ -9,16 +9,19 @@
 //#include <Arduino.h>
 #include <WiFi.h>
 #include <WebServer.h>
+#include <ESP32Servo.h>
 #include "index.h"
 
 // Wifi config
-#define WIFI_SSID "xxx"
-#define WIFI_PASSWD "xxx"
-
+#define WIFI_SSID "WFH"
+#define WIFI_PASSWD "27661005@Calvin3171"
+df
 // Declear parameters
 WebServer server(80);
+Servo motor;
+int flat_panel_pin = 25; int motor_pin = 33;
 int cover_state, flat_panel_state = 0; // 0 is close, 1 is open.
-float motor_angle, motor_temp = 0;
+float motor_angle = 90, motor_temp = 0;
 String local_ip;
 char XML[2048]; // data in xml format.
 
@@ -81,8 +84,12 @@ void handleNotFound() {
 
 void handleOpenCover() {
   if (cover_state == 0) {
+    motor.attach(motor_pin);
+    motor.write(0);
+    delay(2000);
+//    motor.detach();
+    motor_angle = 0;
     cover_state = 1;
-    // open cover
   }
   server.send(200, "text/plain", "");
   Serial.println("Open the cover");
@@ -90,8 +97,12 @@ void handleOpenCover() {
 
 void handleCloseCover() {
   if (cover_state == 1) {
+    motor.attach(motor_pin);
+    motor.write(110);
+    delay(2000);
+//    motor.detach();
+    motor_angle = 90;
     cover_state = 0;
-    // close cover
   }
   server.send(200, "text/plain", "");
   Serial.println("Close the cover");
@@ -100,7 +111,6 @@ void handleCloseCover() {
 void handleOpenFlatPanel() {
   if (flat_panel_state == 0) {
     flat_panel_state = 1;
-    // open cover
   }
   server.send(200, "text/plain", "");
   Serial.println("Open the flat panel");
@@ -109,7 +119,6 @@ void handleOpenFlatPanel() {
 void handleCloseFlatPanel() {
   if (flat_panel_state == 1) {
     flat_panel_state = 0;
-    // close cover
   }
   server.send(200, "text/plain", "");
   Serial.println("Close the flat panel");
@@ -129,6 +138,11 @@ void createWebServer() {
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(flat_panel_pin, OUTPUT);
+  motor.attach(motor_pin);
+  motor.write(110); // close the cover
+  delay(3000);
+  motor.detach();
   Serial.begin(115200);
   while (!connectWifi()) {}
   createWebServer();
@@ -136,4 +150,5 @@ void setup() {
 
 void loop() {
   server.handleClient();
+  digitalWrite(flat_panel_pin, flat_panel_state);
 }
